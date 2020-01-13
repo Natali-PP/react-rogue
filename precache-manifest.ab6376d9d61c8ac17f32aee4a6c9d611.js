63a41362ec8c550587e1fad480bebe4fb672f15e
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2020-01-13 16:33:21 -0300

    Updates

---
+++ precache-manifest.ab6376d9d61c8ac17f32aee4a6c9d611.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94bfd8bafc7e8f7d1a7dfe98b6a6eb3d",
+    "url": "/react-rogue/index.html"
+  },
+  {
+    "revision": "54581d83d79635d6291a",
+    "url": "/react-rogue/static/js/2.9fb010dd.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-rogue/static/js/2.9fb010dd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "58038df565d94b10ef95",
+    "url": "/react-rogue/static/js/main.6e9fe2c6.chunk.js"
+  },
+  {
+    "revision": "fddc21502bbace0f3b3a",
+    "url": "/react-rogue/static/js/runtime-main.d6f0c6e3.js"
+  }
+]);
